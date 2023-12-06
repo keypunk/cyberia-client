2e4b2de23e5f20d7b6b42ac1b80294ac5125801a
keypunk <salehiharun@gmail.com>
2023-12-06 23:13:13 +0100

    initial javafx framework

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.cyberiashop.cyberiaclient {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.cyberiashop.cyberiaclient to javafx.fxml;
+    exports com.cyberiashop.cyberiaclient;
+}
